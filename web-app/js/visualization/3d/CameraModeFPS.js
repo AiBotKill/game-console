fb7e35a45a9ba1ba5846e01048d764da75760df7
joonas <joonas@VarisWareHQ>
2015-02-18 01:11:22 +0200

    Code polishing.

--- web-app/js/visualization/3d/CameraModeFPS.js
+++ web-app/js/visualization/3d/CameraModeFPS.js
@@ -1,9 +1,6 @@
-function CameraModeFPS(){
-    isHUDDrawn = true;
-    setStatusMessage("FPS CAMERA");
-    this.resetCamera();
-    
-    this.refreshCameraMode = function() {
+function CameraModeFPS() {
+
+    this.refreshCameraMode = function () {
         var index = cameraSettings.playerIndex;
         var players = serverData.gamestate.players.length;
         playerTree[cameraSettings.playerIndex].model.remove(camera);
@@ -16,14 +13,18 @@ function CameraModeFPS(){
         setStatusMessage("NEXT BOT");
         this.resetCamera();
     };
-    
-    this.resetCamera = function(){
+
+    this.resetCamera = function () {
         playerTree[cameraSettings.playerIndex].model.add(camera);
         camera.rotation.x = Math.PI * -0.5;
         camera.rotation.y = Math.PI * -0.5;
         camera.rotation.z = Math.PI * -0.5;
         camera.position.set(1, 6, 0);
     }
+    
+    isHUDDrawn = true;
+    setStatusMessage("FPS CAMERA");
+    this.resetCamera();
 }
 
 
