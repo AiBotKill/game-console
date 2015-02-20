d6042260b4d66067733d51cedda6684fdbf4301c
joonas <joonas@VarisWareHQ>
2015-02-20 17:39:49 +0200

    Polishing.

--- web-app/js/visualization/3d/CameraModeExternal.js
+++ web-app/js/visualization/3d/CameraModeExternal.js
@@ -1,28 +1,30 @@
-function CameraModeExternal(){
+function CameraModeExternal() {
 
     this.resetCamera = function () {
-            //orbitingCamera = true;
-            camera.lookAt(scene.position);
-            camera.rotation.x += Math.PI / 2;
-            camera.position.z = CAMERA_PLAYER_Z;
-            isHUDDrawn = false;
-            //camera.translateY(5);
+        //orbitingCamera = true;
+        camera.rotation.x = 1.57;
+        camera.rotation.y = 1.53;
+        camera.lookAt(scene.position);
+        camera.rotation.x += Math.PI / 2;
+        camera.position.z = CAMERA_PLAYER_Z;
+        isHUDDrawn = false;
+        //camera.translateY(5);
     };
 
     this.refreshCameraMode = function () {
         /*
-        var index = cameraSettings.playerIndex;
-        var players = playerTree.length;
-        playerTree[cameraSettings.playerIndex].model.remove(camera);
-        if (index < players - 1) {
-            cameraSettings.playerIndex++;
-        }
-        else {
-            cameraSettings.playerIndex = 0;
-        }
-        setStatusMessage("NEXT BOT");
-        this.resetCamera();
-        */
+         var index = cameraSettings.playerIndex;
+         var players = playerTree.length;
+         playerTree[cameraSettings.playerIndex].model.remove(camera);
+         if (index < players - 1) {
+         cameraSettings.playerIndex++;
+         }
+         else {
+         cameraSettings.playerIndex = 0;
+         }
+         setStatusMessage("NEXT BOT");
+         this.resetCamera();
+         */
     };
     setStatusMessage("EXTERNAL CAMERA");
     this.resetCamera();
