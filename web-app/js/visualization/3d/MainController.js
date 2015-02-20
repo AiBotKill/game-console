fed3de72d4c0ee0c66c71c9d642b3ef7adcc1894
Joonas Nousiainen <Joonas.Nousiainen@liaison.com>
2015-02-20 13:25:38 +0200

    Polishing.

--- web-app/js/visualization/3d/MainController.js
+++ web-app/js/visualization/3d/MainController.js
@@ -499,7 +499,7 @@ function refreshBullets() {
                     y = bullets[i].position.y;
                     xSpeed = bullets[i].velocity.x;
                     ySpeed = bullets[i].velocity.y;
-                    addBullet(x * TILE_WIDTH - (GROUND_X / 2), y * TILE_HEIGHT - (GROUND_Y / 2), xSpeed, ySpeed, bulletId);
+                    addBullet(x - (GROUND_X / 2), y - (GROUND_Y / 2), xSpeed, ySpeed, bulletId);
                 }
                 else {
                     bulletTree[bulletId].model.position.translateX(xSpeed);
