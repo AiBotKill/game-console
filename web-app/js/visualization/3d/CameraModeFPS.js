e2ce194b1cc4907bab4c9a37e3159092544fe064
Joonas Nousiainen <Joonas.Nousiainen@liaison.com>
2015-02-20 12:34:11 +0200

    Polishing.

--- web-app/js/visualization/3d/CameraModeFPS.js
+++ web-app/js/visualization/3d/CameraModeFPS.js
@@ -28,7 +28,15 @@ function CameraModeFPS() {
             cameraSettings.playerIndex = 0;
         }
         setStatusMessage("NEXT BOT");
-        this.resetCamera();
+        if(!playerTree[cameraSettings.playerIndex].destroyed && playerTree[cameraSettings.playerIndex].model){
+            orbitingCamera = false;
+            playerTree[cameraSettings.playerIndex].model.add(camera);
+            camera.rotation.x = Math.PI * -0.5;
+            camera.rotation.y = Math.PI * -0.5;
+            camera.rotation.z = Math.PI * -0.5;
+            camera.position.set(1, 6, 0);
+            isHUDDrawn = true;
+        }
     };
     setStatusMessage("FPS CAMERA");
     this.resetCamera();
