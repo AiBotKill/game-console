0cc16592ab25cc627c77dfa61d88ebd3f1739146
Joonas Nousiainen <Joonas.Nousiainen@liaison.com>
2015-02-20 14:42:14 +0200

    Polishing.

--- web-app/js/visualization/3d/MainController.js
+++ web-app/js/visualization/3d/MainController.js
@@ -327,7 +327,7 @@ function renderHud() {
 }
 
 function createNewBullet(x, y) {
-    var laser = new THREE.Mesh(laserTemplate.geometry, laserTemplate.materials);
+    var laser = new THREE.Mesh(laserTemplate.geometry, laserTemplate.material);
     laser.scale.set(1, 1, 1);
     laser.rotation.x += Math.PI / 2;
     laser.position.x = x;
